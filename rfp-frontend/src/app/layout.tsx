a3c1ba1fa95bfa13d8900d4053c5e532b1086658
vemandhira99 <vemandhira@gmail.com>
2026-04-09 23:34:26 +0530

    pm done

--- rfp-frontend/src/app/layout.tsx
+++ rfp-frontend/src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", inter.variable)}>
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={cn("font-sans")}>
+      <body>{children}</body>
     </html>
   );
 }
